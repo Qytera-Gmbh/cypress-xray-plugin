0ba28c50fd57aad3654178e32af679a1a7d57e67
Sebastian Vollbrecht <sebastian.vollbrecht@qytera.de>
2022-11-22 18:21:47 +0100

    Add package info and basic project files

---
+++ lib/uploader.ts
@@ -0,0 +1,1 @@
+// TODO: write functions for uploading stuff to Xray
---
+++ plugin.js
@@ -0,0 +1,15 @@
+const { beforeRunHook, afterRunHook } = require("./lib/hooks");
+
+module.exports = function (on) {
+  on("before:run", async (details) => {
+    await beforeRunHook(details);
+  });
+
+  on("after:run", async (results) => {
+    await afterRunHook(results);
+  });
+
+  on("after:screenshot", async (details) => {
+    console.log("after:screenshot", details);
+  });
+};
