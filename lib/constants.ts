cba968df546faa91d01e0036f3a376ad6ed1418a
Sebastian Vollbrecht <sebastian.vollbrecht@qytera.de>
2022-11-24 13:12:01 +0100

    Add authentication method

---
+++ lib/constants.ts
@@ -0,0 +1,1 @@
+export const URL_XRAY_V2 = "https://xray.cloud.getxray.app/api/v2/authenticate";
--- lib/uploader.ts
+++ lib/uploader.ts
@@ -1,1 +1,24 @@
 // TODO: write functions for uploading stuff to Xray
+
+import axios from "axios";
+import { URL_XRAY_V2 } from "./constants";
+
+abstract class Uploader {
+    protected static token?: string;
+
+    public async authenticateXray(clientId: string, clientSecret: string) {
+        const response = await axios.post(
+            URL_XRAY_V2,
+            {
+                client_id: clientId,
+                client_secret: clientSecret,
+            },
+            {
+                headers: {
+                    "Content-Type": "application/json",
+                },
+            }
+        );
+        Uploader.token = response.data;
+    }
+}
