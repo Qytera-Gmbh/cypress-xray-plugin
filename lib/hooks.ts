d207b4b3bee5df9f602eb0d3e6bb90153df8aa0c
Sebastian Vollbrecht <sebastian.vollbrecht@qytera.de>
2022-11-24 13:11:34 +0100

    Formatting

--- lib/hooks.ts
+++ lib/hooks.ts
@@ -1,7 +1,7 @@
 export async function beforeRunHook(details) {
-  console.log("after:run", details);
+    console.log("after:run", details);
 }
 
 export async function afterRunHook(results) {
-  console.log("after:run", results);
+    console.log("after:run", results);
 }
