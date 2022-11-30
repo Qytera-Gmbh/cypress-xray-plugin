874b23b86a4c725779b3a06efe64486e538645d0
Sebastian Vollbrecht <sebastian.vollbrecht@qytera.de>
2022-11-30 17:47:05 +0100

    Add OpenSSL configurability

--- src/constants.ts
+++ src/constants.ts
@@ -1,11 +1,25 @@
-// Jira variables.
+// ================================= //
+// | Jira Configuration            | //
+// ================================= //
 export const ENV_XRAY_PROJECT_KEY = "XRAY_PROJECT_KEY";
 export const ENV_XRAY_EXECUTION_ISSUE_KEY = "XRAY_EXECUTION_ISSUE_KEY";
+// ================================= //
+// | Xray Server Configuration     | //
+// ================================= //
+export const ENV_XRAY_API_URL = "XRAY_API_URL";
 // Basic authentication.
 export const ENV_XRAY_USERNAME = "XRAY_USERNAME";
 export const ENV_XRAY_PASSWORD = "XRAY_PASSWORD";
 // API token authentication.
 export const ENV_XRAY_API_TOKEN = "XRAY_API_TOKEN";
+// ================================= //
+// | Xray Cloud Configuration      | //
+// ================================= //
 // JWT-based authentication.
 export const ENV_XRAY_CLIENT_ID = "XRAY_CLIENT_ID";
 export const ENV_XRAY_CLIENT_SECRET = "XRAY_CLIENT_SECRET";
+// ================================= //
+// | OpenSSL Configuration         | //
+// ================================= //
+export const ENV_OPENSSL_ROOT_CA_PATH = "OPENSSL_ROOT_CA_PATH";
+export const ENV_OPENSSL_SECURE_OPTIONS = "OPENSSL_SECURE_OPTIONS";
---
+++ src/https/requests.ts
@@ -0,0 +1,48 @@
+import axios, { AxiosRequestConfig, AxiosResponse } from "axios";
+import { readFileSync } from "fs";
+import { Agent } from "https";
+import {
+    ENV_OPENSSL_ROOT_CA_PATH,
+    ENV_OPENSSL_SECURE_OPTIONS,
+} from "../constants";
+import { UploadContext } from "../context";
+
+export class Requests {
+    private static AGENT = new Agent({
+        ca:
+            ENV_OPENSSL_ROOT_CA_PATH in UploadContext.ENV
+                ? Requests.readCertificate(
+                      UploadContext.ENV[ENV_OPENSSL_ROOT_CA_PATH]
+                  )
+                : undefined,
+        secureOptions:
+            ENV_OPENSSL_SECURE_OPTIONS in UploadContext.ENV
+                ? UploadContext.ENV[ENV_OPENSSL_SECURE_OPTIONS]
+                : undefined,
+    });
+
+    private static readCertificate(path: string): Buffer {
+        return readFileSync(path);
+    }
+
+    public static get(
+        url: string,
+        config?: AxiosRequestConfig
+    ): Promise<AxiosResponse> {
+        return axios.get(url, {
+            ...config,
+            httpsAgent: Requests.AGENT,
+        });
+    }
+
+    public static post(
+        url: string,
+        data?: any,
+        config?: AxiosRequestConfig
+    ): Promise<AxiosResponse> {
+        return axios.post(url, data, {
+            ...config,
+            httpsAgent: Requests.AGENT,
+        });
+    }
+}
