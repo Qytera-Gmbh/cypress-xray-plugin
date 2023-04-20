9d877293af1ea801defdf71254d38796d36856c5
Sebastian Vollbrecht <christoph_sebastian.vollbrecht@stud.tu-darmstadt.de>
2023-04-20 19:39:39 +0200

    Delete outdated publish script

--- publish.sh
+++
@@ -1,9 +0,0 @@
-# Remove existing directory to not publish outdated stuff.
-npx shx rm -r dist/
-# Compile Typescript code to vanilla JavaScript + type declarations.
-npm run build
-# Prepare package inside the build directory.
-npx shx cp package.json README.md LICENSE.md dist/
-# Publish from inside the build directory.
-cd dist/
-npm publish
