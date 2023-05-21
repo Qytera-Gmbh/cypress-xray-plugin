4ca87afd8142a66915384682e783db4a01e0a0e3
Sebastian Vollbrecht <christoph_sebastian.vollbrecht@stud.tu-darmstadt.de>
2023-05-21 15:05:18 +0200

    Fix plugin context providing mutable defaults

--- src/context.ts
+++ src/context.ts
@@ -1,56 +1,48 @@
-import {
-    CucumberOptions,
-    JiraOptions,
-    OpenSSLOptions,
-    Options,
-    PluginContext,
-    PluginOptions,
-    XrayOptions,
-} from "./types/plugin";
+import { Options, PluginContext } from "./types/plugin";
 
 export let CONTEXT: PluginContext = null;
 
 export function initContext(config: Options) {
+    // Set some default values for constant options.
     CONTEXT = {
         config: {
-            jira: { ...DEFAULT_OPTIONS_JIRA, ...config.jira },
-            plugin: { ...DEFAULT_OPTIONS_PLUGIN, ...config.plugin },
-            xray: { ...DEFAULT_OPTIONS_Xray, ...config.xray },
-            cucumber: { ...DEFAULT_OPTIONS_CUCUMBER, ...config.cucumber },
-            openSSL: { ...DEFAULT_OPTIONS_OPEN_SSL, ...config.openSSL },
+            jira: {
+                projectKey: config.jira?.projectKey,
+                attachVideos: config.jira?.attachVideos || false,
+                createTestIssues: config.jira?.createTestIssues || true,
+                testExecutionIssueDescription: config.jira?.testExecutionIssueDescription,
+                testExecutionIssueKey: config.jira?.testExecutionIssueKey,
+                testExecutionIssueSummary: config.jira?.testExecutionIssueSummary,
+                testPlanIssueKey: config.jira?.testPlanIssueKey,
+                url: config.jira?.url,
+            },
+            plugin: {
+                overwriteIssueSummary: config.plugin?.overwriteIssueSummary || false,
+                normalizeScreenshotNames: config.plugin?.normalizeScreenshotNames || false,
+                debug: config.plugin?.debug || false,
+            },
+            xray: {
+                statusFailed: config.xray?.statusFailed,
+                statusPassed: config.xray?.statusPassed,
+                statusPending: config.xray?.statusPending,
+                steps: {
+                    maxLengthAction: config.xray?.steps?.maxLengthAction || 8000,
+                    update: config.xray?.steps?.update || true,
+                },
+                testType: config.xray?.testType || "Manual",
+                uploadResults: config.xray?.uploadResults || true,
+                uploadScreenshots: config.xray?.uploadScreenshots || true,
+            },
+            cucumber: {
+                featureFileExtension: config.cucumber?.featureFileExtension,
+                downloadFeatures: config.cucumber?.downloadFeatures || false,
+                issues: undefined,
+                uploadFeatures: config.cucumber?.uploadFeatures || false,
+            },
+            openSSL: {
+                rootCAPath: config.openSSL?.rootCAPath,
+                secureOptions: config.openSSL?.secureOptions,
+            },
         },
     };
 }
-
-// Set some default values for constant options.
-
-const DEFAULT_OPTIONS_JIRA: JiraOptions = {
-    projectKey: null,
-    attachVideos: false,
-    createTestIssues: true,
-};
-const DEFAULT_OPTIONS_PLUGIN: PluginOptions = {
-    overwriteIssueSummary: false,
-    normalizeScreenshotNames: false,
-    debug: false,
-};
-
-const DEFAULT_OPTIONS_Xray: XrayOptions = {
-    uploadResults: true,
-    uploadScreenshots: true,
-    statusPassed: "PASSED",
-    statusFailed: "FAILED",
-    testType: "Manual",
-    steps: {
-        update: true,
-        maxLengthAction: 8000,
-    },
-};
-
-const DEFAULT_OPTIONS_CUCUMBER: CucumberOptions = {
-    featureFileExtension: null,
-    uploadFeatures: false,
-    downloadFeatures: false,
-};
-
-const DEFAULT_OPTIONS_OPEN_SSL: OpenSSLOptions = {};
