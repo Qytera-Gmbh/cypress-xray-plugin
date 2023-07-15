e369fee7f01529b6c42ff519abe676f400dce3eb
Basti <18561435+csvtuda@users.noreply.github.com>
2023-07-15 13:31:39 +0200

    Restrict exports to members exported in `index.ts` (#109)

---
+++ index.ts
@@ -0,0 +1,9 @@
+import { addXrayResultUpload, configureXrayPlugin, syncFeatureFile } from "./src/plugin";
+import { Options } from "./src/types/plugin";
+
+export {
+    Options as CypressXrayPluginOptions,
+    addXrayResultUpload,
+    configureXrayPlugin,
+    syncFeatureFile,
+};
