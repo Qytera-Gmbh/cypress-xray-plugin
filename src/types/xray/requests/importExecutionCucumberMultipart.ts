01baa88e5a3a31e7f346d464f1144ad4a3be2dd3
Sebastian Vollbrecht <christoph_sebastian.vollbrecht@stud.tu-darmstadt.de>
2023-07-15 19:18:55 +0200

    Beautify Cucumber multipart types

--- src/types/xray/requests/importExecutionCucumberMultipart.ts
+++ src/types/xray/requests/importExecutionCucumberMultipart.ts
@@ -3,6 +3,16 @@ import {
     CucumberMultipartInfoServer,
 } from "./importExecutionCucumberMultipartInfo";
 
+/*
+ * There is unfortunately no official Cucumber JSON report scheme available anywhere, as stated
+ * here: https://github.com/cucumber/messages#lack-of-a-schema
+ *
+ * Therefore, the types in here are a mix of the following sources:
+ * - https://github.com/cucumber/cucumber-js/blob/main/src/formatter/json_formatter.ts
+ * - https://github.com/cucumber/json-formatter/blob/main/go/json_elements.go
+ * - https://github.com/cucumber/cucumber-json-converter/blob/main/src/CucumberJson.ts
+ */
+
 export type CucumberMultipart<CucumberMultipartInfoType> = {
     features: CucumberMultipartFeature[];
     info: CucumberMultipartInfoType;
@@ -10,63 +20,68 @@ export type CucumberMultipart<CucumberMultipartInfoType> = {
 export type CucumberMultipartServer = CucumberMultipart<CucumberMultipartInfoServer>;
 export type CucumberMultipartCloud = CucumberMultipart<CucumberMultipartInfoCloud>;
 export interface CucumberMultipartFeature {
-    uri: string;
-    id?: string;
-    line?: number;
-    keyword: string;
-    name: string;
-    description?: string;
+    description: string;
     elements: CucumberMultipartElement[];
+    id: string;
+    keyword: string;
+    line: number;
+    name: string;
     tags?: CucumberMultipartTag[];
+    uri: string;
 }
 export interface CucumberMultipartElement {
-    start_timestamp?: string;
-    line: number;
-    id?: string;
-    type: "background" | "scenario";
-    keyword: string;
-    name: string;
     description: string;
+    id?: string;
+    keyword: string;
+    line: number;
+    name: string;
     before?: CucumberMultipartHook[];
     steps: CucumberMultipartStep[];
     after?: CucumberMultipartHook[];
+    type: "background" | "scenario";
     tags?: CucumberMultipartTag[];
+}
+export interface CucumberMultipartStep {
+    doc_string?: CucumberMultipartDocString;
+    embeddings?: CucumberMultipartEmbedding[];
+    keyword: string;
+    line: number;
+    match?: CucumberMultipartMatch;
+    name: string;
+    output?: string;
+    result: CucumberMultipartStepResult;
+    rows?: CucumberMultipartDataTableRow[];
+}
+export interface CucumberMultipartDocString {
+    content_type: string;
+    line: number;
+    value: string;
+}
+export interface CucumberMultipartDataTableRow {
+    cells: string[];
+}
+export interface CucumberMultipartStepResult {
+    duration?: number;
+    status: "passed" | "failed" | "skipped" | "undefined" | "pending" | "unknown";
+    error_message?: string;
+}
+export interface CucumberMultipartMatch {
+    location: string;
+    arguments?: CucumberMultipartArgument[];
+}
+export interface CucumberMultipartTag {
+    name: string;
+    line?: number;
 }
 export interface CucumberMultipartHook {
     match?: CucumberMultipartMatch;
-    result: CucumberMultipartResult;
-}
-export interface CucumberMultipartMatch {
-    location?: string;
-    arguments?: CucumberMultipartArgument[];
+    result: CucumberMultipartStepResult;
 }
 export interface CucumberMultipartArgument {
     value: string;
     offset: number;
 }
-export interface CucumberMultipartResult {
-    duration?: number;
-    status: "passed" | "failed" | "skipped" | "undefined" | "pending" | "unknown";
-    error_message?: string;
-}
-export interface CucumberMultipartStep {
-    keyword: string;
-    line: number;
-    match?: CucumberMultipartMatch;
-    name: string;
-    result: CucumberMultipartResult;
-    doc_string?: CucumberMultipartDocString;
-    rows?: CucumberMultipartDataTableRow[];
-}
-export interface CucumberMultipartDocString {
-    line: number;
-    value: string;
-    content_type?: string;
-}
-export interface CucumberMultipartDataTableRow {
-    cells: string[];
-}
-export interface CucumberMultipartTag {
-    name: string;
-    line?: number;
+export interface CucumberMultipartEmbedding {
+    data: string;
+    mime_type: string;
 }
