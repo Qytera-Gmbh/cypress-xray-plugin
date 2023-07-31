f5a7f911e8adf2f4b03fd4f27e3946d0d64ba69e
Sebastian Vollbrecht <christoph_sebastian.vollbrecht@stud.tu-darmstadt.de>
2023-07-31 20:56:11 +0200

    Fix `cypress-xray-plugin/register` export

--- register.ts
+++
@@ -1,6 +0,0 @@
-/// <reference types="cypress" />
-
-// Currently not in use.
-Cypress.on("test:after:run", (test: Cypress.ObjectLike, runnable: Mocha.Test) => {
-    // console.log("test:after:run", test, runnable);
-});
