849b315cfb35df4940df7c1172774c05019ea9d7
Sebastian Vollbrecht <christoph_sebastian.vollbrecht@stud.tu-darmstadt.de>
2023-08-13 12:33:38 +0200

    Add tsdoc

--- src/util/arrays.ts
+++ src/util/arrays.ts
@@ -1,3 +1,10 @@
+/**
+ * Checks whether two arrays contain exactly the same elements, regardless of order.
+ *
+ * @param a the first array
+ * @param b the second array
+ * @returns if `a` and `b` contain the same elements
+ */
 export function equalsIgnoreOrder<T>(a: T[], b: T[]): boolean {
     if (a.length !== b.length) {
         return false;
