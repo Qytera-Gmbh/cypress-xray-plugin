9b7ad8503e8e527e30a543e02095f1f0495b700a
Basti <18561435+csvtuda@users.noreply.github.com>
2023-09-17 17:58:07 +0200

    Remove `xray.steps` options (#180)

--- src/constants.ts
+++ src/constants.ts
@@ -32,8 +32,6 @@ export const ENV_XRAY_STATUS_FAILED = "XRAY_STATUS_FAILED";
 export const ENV_XRAY_STATUS_PASSED = "XRAY_STATUS_PASSED";
 export const ENV_XRAY_STATUS_PENDING = "XRAY_STATUS_PENDING";
 export const ENV_XRAY_STATUS_SKIPPED = "XRAY_STATUS_SKIPPED";
-export const ENV_XRAY_STEPS_MAX_LENGTH_ACTION = "XRAY_STEPS_MAX_LENGTH_ACTION";
-export const ENV_XRAY_STEPS_UPDATE = "XRAY_STEPS_UPDATE";
 export const ENV_XRAY_UPLOAD_RESULTS = "XRAY_UPLOAD_RESULTS";
 export const ENV_XRAY_UPLOAD_SCREENSHOTS = "XRAY_UPLOAD_SCREENSHOTS";
 // ============================================================================================== //
