4d82ce10f5602f71ac037147a9f54a62db56d1e5
Sebastian Vollbrecht <christoph_sebastian.vollbrecht@stud.tu-darmstadt.de>
2023-10-21 12:56:20 +0200

    Add property extraction utility

---
+++ src/util/extraction.spec.ts
@@ -0,0 +1,146 @@
+import { expect } from "chai";
+import { extractArrayOfStrings, extractNestedString, extractString } from "./extraction";
+
+describe("extraction", () => {
+    describe("extractString", () => {
+        it("extracts string properties", () => {
+            const data = {
+                x: "nice to meet you",
+            };
+            expect(extractString(data, "x")).to.eq("nice to meet you");
+        });
+        it("throws if data is not an object", () => {
+            expect(() => extractString(5, "x")).to.throw(
+                "Expected an object containing property 'x', but got: 5"
+            );
+        });
+        it("throws if data is null", () => {
+            expect(() => extractString(null, "x")).to.throw(
+                "Expected an object containing property 'x', but got: null"
+            );
+        });
+        it("throws if data is undefined", () => {
+            expect(() => extractString(undefined, "x")).to.throw(
+                "Expected an object containing property 'x', but got: undefined"
+            );
+        });
+        it("throws if data does not contain the property", () => {
+            const data = {
+                x: "nice to meet you",
+            };
+            expect(() => extractString(data, "missing")).to.throw(
+                'Expected an object containing property \'missing\', but got: {"x":"nice to meet you"}'
+            );
+        });
+        it("throws if the property is not a string value", () => {
+            const data = {
+                x: 5,
+            };
+            expect(() => extractString(data, "x")).to.throw("Value is not of type string: 5");
+        });
+    });
+
+    describe("extractArrayOfStrings", () => {
+        it("extracts string array properties", () => {
+            const data = {
+                x: ["nice", "to", "meet", "you"],
+            };
+            expect(extractArrayOfStrings(data, "x")).to.deep.eq(["nice", "to", "meet", "you"]);
+        });
+        it("throws if data is not an object", () => {
+            expect(() => extractArrayOfStrings(5, "x")).to.throw(
+                "Expected an object containing property 'x', but got: 5"
+            );
+        });
+        it("throws if data is null", () => {
+            expect(() => extractArrayOfStrings(null, "x")).to.throw(
+                "Expected an object containing property 'x', but got: null"
+            );
+        });
+        it("throws if data is undefined", () => {
+            expect(() => extractArrayOfStrings(undefined, "x")).to.throw(
+                "Expected an object containing property 'x', but got: undefined"
+            );
+        });
+        it("throws if data does not contain the property", () => {
+            const data = {
+                x: ["nice", "to", "meet", "you"],
+            };
+            expect(() => extractArrayOfStrings(data, "missing")).to.throw(
+                'Expected an object containing property \'missing\', but got: {"x":["nice","to","meet","you"]}'
+            );
+        });
+        it("throws if the property is not an array value", () => {
+            const data = {
+                x: "good morning",
+            };
+            expect(() => extractArrayOfStrings(data, "x")).to.throw(
+                'Value is not an array of type string: "good morning"'
+            );
+        });
+        it("throws if the property is not a string array value", () => {
+            const data = {
+                x: ["good", "morning", "my", 42, "friends"],
+            };
+            expect(() => extractArrayOfStrings(data, "x")).to.throw(
+                'Value is not an array of type string: ["good","morning","my",42,"friends"]'
+            );
+        });
+    });
+
+    describe("extractNestedString", () => {
+        it("extracts nested string properties", () => {
+            const data = {
+                a: {
+                    b: {
+                        c: {
+                            d: "hello",
+                        },
+                    },
+                },
+            };
+            expect(extractNestedString(data, ["a", "b", "c", "d"])).to.eq("hello");
+        });
+        it("throws if data is not an object", () => {
+            expect(() => extractNestedString(5, ["x"])).to.throw(
+                "Expected an object containing property 'x', but got: 5"
+            );
+        });
+        it("throws if data is null", () => {
+            expect(() => extractNestedString(null, ["x"])).to.throw(
+                "Expected an object containing property 'x', but got: null"
+            );
+        });
+        it("throws if data is undefined", () => {
+            expect(() => extractNestedString(undefined, ["x"])).to.throw(
+                "Expected an object containing property 'x', but got: undefined"
+            );
+        });
+        it("throws if a nested property is not an object value", () => {
+            const data = {
+                a: {
+                    b: {
+                        c: "surprise",
+                    },
+                },
+            };
+            expect(() => extractNestedString(data, ["a", "b", "c", "d"])).to.throw(
+                "Expected an object containing property 'd', but got: \"surprise\""
+            );
+        });
+        it("throws if the final property is not a string value", () => {
+            const data = {
+                a: {
+                    b: {
+                        c: {
+                            d: 42,
+                        },
+                    },
+                },
+            };
+            expect(() => extractNestedString(data, ["a", "b", "c", "d"])).to.throw(
+                "Value is not of type string: 42"
+            );
+        });
+    });
+});
---
+++ src/util/extraction.ts
@@ -0,0 +1,83 @@
+import { StringMap } from "../types/util";
+
+/**
+ * Extracts a string property from an object.
+ *
+ * @param data - the object
+ * @param propertyName - the property to access
+ * @returns the property's string value
+ * @throws if `data` is not an object or does not contain a string property `propertyName`
+ */
+export function extractString(data: unknown, propertyName: string): string {
+    verifyIsObjectWithProperty(data, propertyName);
+    const value = data[propertyName];
+    if (typeof value !== "string") {
+        throw new Error(`Value is not of type string: ${value}`);
+    }
+    return value;
+}
+
+/**
+ * Extracts a string array property from an object.
+ *
+ * @param data - the object
+ * @param propertyName - the property to access
+ * @returns the property's string array value
+ * @throws if `data` is not an object or does not contain a string array property `propertyName`
+ */
+export function extractArrayOfStrings(data: unknown, propertyName: string): string[] {
+    verifyIsObjectWithProperty(data, propertyName);
+    const value = data[propertyName];
+    if (!Array.isArray(value) || value.some((element) => typeof element !== "string")) {
+        throw new Error(`Value is not an array of type string: ${JSON.stringify(value)}`);
+    }
+    return value;
+}
+
+/**
+ * Recursively extracts a string property from an object. The array of property names is used to
+ * recursively access the nested property values of the provided data object. The last nested
+ * object must then contain a string property matching the last provided property name.
+ *
+ * @example
+ * ```ts
+ * const data = {
+ *   a: {
+ *     b: {
+ *       c: {
+ *         d: "hello"
+ *       }
+ *     }
+ *   }
+ * };
+ * console.log(extractNestedString(data, ["a", "b", "c", "d"]));
+ * // hello
+ * ```
+ *
+ * @param data - the object
+ * @param propertyName - the property to access
+ * @returns the property's string value
+ * @throws if `data` is not an object or does not contain a nested string property `propertyName`
+ */
+export function extractNestedString(data: unknown, propertyNames: [string, ...string[]]): string {
+    let currentData: unknown = data;
+    for (let i = 0; i < propertyNames.length - 1; i++) {
+        const property = propertyNames[i];
+        verifyIsObjectWithProperty(currentData, property);
+        currentData = currentData[property];
+    }
+    return extractString(currentData, propertyNames[propertyNames.length - 1]);
+}
+
+function verifyIsObjectWithProperty(
+    data: unknown,
+    propertyName: string
+): asserts data is StringMap<unknown> {
+    if (typeof data !== "object" || data === null || !(propertyName in data)) {
+        throw new Error(
+            `Expected an object containing property '${propertyName}', but got: ${JSON.stringify(
+                data
+            )}`
+        );
+    }
+}
