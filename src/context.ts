8c6b16497eaf17bf079139bcca021a72c1c3a136
Sebastian Vollbrecht <christoph_sebastian.vollbrecht@stud.tu-darmstadt.de>
2023-11-12 02:21:48 +0100

    Replace environment variable names with new type

--- src/context.ts
+++ src/context.ts
@@ -4,48 +4,11 @@ import { JiraClientServer } from "./client/jira/jiraClientServer";
 import { XrayClientCloud } from "./client/xray/xrayClientCloud";
 import { XrayClientServer } from "./client/xray/xrayClientServer";
 import {
-    ENV_CUCUMBER_DOWNLOAD_FEATURES,
-    ENV_CUCUMBER_FEATURE_FILE_EXTENSION,
-    ENV_CUCUMBER_UPLOAD_FEATURES,
-    ENV_JIRA_API_TOKEN,
-    ENV_JIRA_ATTACH_VIDEOS,
-    ENV_JIRA_FIELDS_DESCRIPTION,
-    ENV_JIRA_FIELDS_LABELS,
-    ENV_JIRA_FIELDS_SUMMARY,
-    ENV_JIRA_FIELDS_TEST_ENVIRONMENTS,
-    ENV_JIRA_FIELDS_TEST_PLAN,
-    ENV_JIRA_FIELDS_TEST_TYPE,
-    ENV_JIRA_PASSWORD,
-    ENV_JIRA_PROJECT_KEY,
-    ENV_JIRA_TEST_EXECUTION_ISSUE_DESCRIPTION,
-    ENV_JIRA_TEST_EXECUTION_ISSUE_KEY,
-    ENV_JIRA_TEST_EXECUTION_ISSUE_SUMMARY,
-    ENV_JIRA_TEST_EXECUTION_ISSUE_TYPE,
-    ENV_JIRA_TEST_PLAN_ISSUE_KEY,
-    ENV_JIRA_TEST_PLAN_ISSUE_TYPE,
-    ENV_JIRA_URL,
-    ENV_JIRA_USERNAME,
-    ENV_OPENSSL_ROOT_CA_PATH,
-    ENV_OPENSSL_SECURE_OPTIONS,
-    ENV_PLUGIN_DEBUG,
-    ENV_PLUGIN_ENABLED,
-    ENV_PLUGIN_LOG_DIRECTORY,
-    ENV_PLUGIN_NORMALIZE_SCREENSHOT_NAMES,
-    ENV_XRAY_CLIENT_ID,
-    ENV_XRAY_CLIENT_SECRET,
-    ENV_XRAY_STATUS_FAILED,
-    ENV_XRAY_STATUS_PASSED,
-    ENV_XRAY_STATUS_PENDING,
-    ENV_XRAY_STATUS_SKIPPED,
-    ENV_XRAY_TEST_ENVIRONMENTS,
-    ENV_XRAY_UPLOAD_RESULTS,
-    ENV_XRAY_UPLOAD_SCREENSHOTS,
-} from "./constants";
-import {
     CucumberPreprocessorArgs,
     CucumberPreprocessorExports,
     importOptionalDependency,
 } from "./dependencies";
+import { ENV_NAMES } from "./env";
 import { logDebug, logInfo } from "./logging/logging";
 import { CachingJiraFieldRepository } from "./repository/jira/fields/jiraFieldRepository";
 import { JiraIssueFetcher, JiraIssueFetcherCloud } from "./repository/jira/fields/jiraIssueFetcher";
@@ -92,19 +55,19 @@ export function initJiraOptions(
     env: Cypress.ObjectLike,
     options: Options["jira"]
 ): InternalJiraOptions {
-    const projectKey = parse(env, ENV_JIRA_PROJECT_KEY, asString) ?? options.projectKey;
+    const projectKey = parse(env, ENV_NAMES.jira.projectKey, asString) ?? options.projectKey;
     if (!projectKey) {
         throw new Error("Plugin misconfiguration: Jira project key was not set");
     }
     const testExecutionIssueKey =
-        parse(env, ENV_JIRA_TEST_EXECUTION_ISSUE_KEY, asString) ?? options.testExecutionIssueKey;
+        parse(env, ENV_NAMES.jira.testExecutionIssueKey, asString) ?? options.testExecutionIssueKey;
     if (testExecutionIssueKey && !testExecutionIssueKey.startsWith(projectKey)) {
         throw new Error(
             `Plugin misconfiguration: test execution issue key ${testExecutionIssueKey} does not belong to project ${projectKey}`
         );
     }
     const testPlanIssueKey =
-        parse(env, ENV_JIRA_TEST_PLAN_ISSUE_KEY, asString) ?? options.testPlanIssueKey;
+        parse(env, ENV_NAMES.jira.testPlanIssueKey, asString) ?? options.testPlanIssueKey;
     if (testPlanIssueKey && !testPlanIssueKey.startsWith(projectKey)) {
         throw new Error(
             `Plugin misconfiguration: test plan issue key ${testPlanIssueKey} does not belong to project ${projectKey}`
@@ -112,37 +75,40 @@ export function initJiraOptions(
     }
     return {
         attachVideos:
-            parse(env, ENV_JIRA_ATTACH_VIDEOS, asBoolean) ?? options.attachVideos ?? false,
+            parse(env, ENV_NAMES.jira.attachVideos, asBoolean) ?? options.attachVideos ?? false,
         fields: {
             description:
-                parse(env, ENV_JIRA_FIELDS_DESCRIPTION, asString) ?? options.fields?.description,
-            labels: parse(env, ENV_JIRA_FIELDS_LABELS, asString) ?? options.fields?.labels,
-            summary: parse(env, ENV_JIRA_FIELDS_SUMMARY, asString) ?? options.fields?.summary,
+                parse(env, ENV_NAMES.jira.fields.description, asString) ??
+                options.fields?.description,
+            labels: parse(env, ENV_NAMES.jira.fields.labels, asString) ?? options.fields?.labels,
+            summary: parse(env, ENV_NAMES.jira.fields.summary, asString) ?? options.fields?.summary,
             testEnvironments:
-                parse(env, ENV_JIRA_FIELDS_TEST_ENVIRONMENTS, asString) ??
+                parse(env, ENV_NAMES.jira.fields.testEnvironments, asString) ??
                 options.fields?.testEnvironments,
-            testPlan: parse(env, ENV_JIRA_FIELDS_TEST_PLAN, asString) ?? options.fields?.testPlan,
-            testType: parse(env, ENV_JIRA_FIELDS_TEST_TYPE, asString) ?? options.fields?.testType,
+            testPlan:
+                parse(env, ENV_NAMES.jira.fields.testPlan, asString) ?? options.fields?.testPlan,
+            testType:
+                parse(env, ENV_NAMES.jira.fields.testType, asString) ?? options.fields?.testType,
         },
         projectKey: projectKey,
         testExecutionIssueDescription:
-            parse(env, ENV_JIRA_TEST_EXECUTION_ISSUE_DESCRIPTION, asString) ??
+            parse(env, ENV_NAMES.jira.testExecutionIssueDescription, asString) ??
             options.testExecutionIssueDescription,
         testExecutionIssueDetails: { subtask: false },
         testExecutionIssueKey: testExecutionIssueKey,
         testExecutionIssueSummary:
-            parse(env, ENV_JIRA_TEST_EXECUTION_ISSUE_SUMMARY, asString) ??
+            parse(env, ENV_NAMES.jira.testExecutionIssueSummary, asString) ??
             options.testExecutionIssueSummary,
         testExecutionIssueType:
-            parse(env, ENV_JIRA_TEST_EXECUTION_ISSUE_TYPE, asString) ??
+            parse(env, ENV_NAMES.jira.testExecutionIssueType, asString) ??
             options.testExecutionIssueType ??
             "Test Execution",
         testPlanIssueKey: testPlanIssueKey,
         testPlanIssueType:
-            parse(env, ENV_JIRA_TEST_PLAN_ISSUE_TYPE, asString) ??
+            parse(env, ENV_NAMES.jira.testPlanIssueType, asString) ??
             options.testPlanIssueType ??
             "Test Plan",
-        url: parse(env, ENV_JIRA_URL, asString) ?? options.url,
+        url: parse(env, ENV_NAMES.jira.url, asString) ?? options.url,
     };
 }
 
@@ -160,12 +126,12 @@ export function initPluginOptions(
     options: Options["plugin"]
 ): InternalPluginOptions {
     return {
-        debug: parse(env, ENV_PLUGIN_DEBUG, asBoolean) ?? options?.debug ?? false,
-        enabled: parse(env, ENV_PLUGIN_ENABLED, asBoolean) ?? options?.enabled ?? true,
+        debug: parse(env, ENV_NAMES.plugin.debug, asBoolean) ?? options?.debug ?? false,
+        enabled: parse(env, ENV_NAMES.plugin.enabled, asBoolean) ?? options?.enabled ?? true,
         logDirectory:
-            parse(env, ENV_PLUGIN_LOG_DIRECTORY, asString) ?? options?.logDirectory ?? "logs",
+            parse(env, ENV_NAMES.plugin.logDirectory, asString) ?? options?.logDirectory ?? "logs",
         normalizeScreenshotNames:
-            parse(env, ENV_PLUGIN_NORMALIZE_SCREENSHOT_NAMES, asBoolean) ??
+            parse(env, ENV_NAMES.plugin.normalizeScreenshotNames, asBoolean) ??
             options?.normalizeScreenshotNames ??
             false,
     };
@@ -186,17 +152,20 @@ export function initXrayOptions(
 ): InternalXrayOptions {
     return {
         status: {
-            failed: parse(env, ENV_XRAY_STATUS_FAILED, asString) ?? options?.status?.failed,
-            passed: parse(env, ENV_XRAY_STATUS_PASSED, asString) ?? options?.status?.passed,
-            pending: parse(env, ENV_XRAY_STATUS_PENDING, asString) ?? options?.status?.pending,
-            skipped: parse(env, ENV_XRAY_STATUS_SKIPPED, asString) ?? options?.status?.skipped,
+            failed: parse(env, ENV_NAMES.xray.status.failed, asString) ?? options?.status?.failed,
+            passed: parse(env, ENV_NAMES.xray.status.passed, asString) ?? options?.status?.passed,
+            pending:
+                parse(env, ENV_NAMES.xray.status.pending, asString) ?? options?.status?.pending,
+            skipped:
+                parse(env, ENV_NAMES.xray.status.skipped, asString) ?? options?.status?.skipped,
         },
         testEnvironments:
-            parse(env, ENV_XRAY_TEST_ENVIRONMENTS, asArrayOfStrings) ?? options?.testEnvironments,
+            parse(env, ENV_NAMES.xray.testEnvironments, asArrayOfStrings) ??
+            options?.testEnvironments,
         uploadResults:
-            parse(env, ENV_XRAY_UPLOAD_RESULTS, asBoolean) ?? options?.uploadResults ?? true,
+            parse(env, ENV_NAMES.xray.uploadResults, asBoolean) ?? options?.uploadResults ?? true,
         uploadScreenshots:
-            parse(env, ENV_XRAY_UPLOAD_SCREENSHOTS, asBoolean) ??
+            parse(env, ENV_NAMES.xray.uploadScreenshots, asBoolean) ??
             options?.uploadScreenshots ??
             true,
     };
@@ -217,7 +186,7 @@ export async function initCucumberOptions(
 ): Promise<InternalCucumberOptions | undefined> {
     // Check if the user has chosen to upload Cucumber results, too.
     const featureFileExtension =
-        parse(config.env, ENV_CUCUMBER_FEATURE_FILE_EXTENSION, asString) ??
+        parse(config.env, ENV_NAMES.cucumber.featureFileExtension, asString) ??
         options?.featureFileExtension;
     // If the user has chosen to do so, we need to make sure they configured the Cucumber
     // preprocessor JSON report as well. Otherwise, results upload will not work.
@@ -268,13 +237,13 @@ export async function initCucumberOptions(
         }
         return {
             downloadFeatures:
-                parse(config.env, ENV_CUCUMBER_DOWNLOAD_FEATURES, asBoolean) ??
+                parse(config.env, ENV_NAMES.cucumber.downloadFeatures, asBoolean) ??
                 options?.downloadFeatures ??
                 false,
             featureFileExtension: featureFileExtension,
             preprocessor: preprocessorConfiguration,
             uploadFeatures:
-                parse(config.env, ENV_CUCUMBER_UPLOAD_FEATURES, asBoolean) ??
+                parse(config.env, ENV_NAMES.cucumber.uploadFeatures, asBoolean) ??
                 options?.uploadFeatures ??
                 false,
         };
@@ -296,8 +265,8 @@ export function initOpenSSLOptions(
     options: Options["openSSL"]
 ): InternalOpenSSLOptions {
     return {
-        rootCAPath: parse(env, ENV_OPENSSL_ROOT_CA_PATH, asString) ?? options?.rootCAPath,
-        secureOptions: parse(env, ENV_OPENSSL_SECURE_OPTIONS, asInt) ?? options?.secureOptions,
+        rootCAPath: parse(env, ENV_NAMES.openSSL.rootCAPath, asString) ?? options?.rootCAPath,
+        secureOptions: parse(env, ENV_NAMES.openSSL.secureOptions, asInt) ?? options?.secureOptions,
     };
 }
 
@@ -313,23 +282,29 @@ export async function initClients(
             `)
         );
     }
-    if (ENV_JIRA_USERNAME in env && ENV_JIRA_API_TOKEN in env) {
+    if (
+        ENV_NAMES.authentication.jira.username in env &&
+        ENV_NAMES.authentication.jira.apiToken in env
+    ) {
         // Jira cloud authentication: username (Email) and token.
         logInfo("Jira username and API token found. Setting up Jira cloud basic auth credentials");
         const credentials = new BasicAuthCredentials(
-            env[ENV_JIRA_USERNAME],
-            env[ENV_JIRA_API_TOKEN]
+            env[ENV_NAMES.authentication.jira.username],
+            env[ENV_NAMES.authentication.jira.apiToken]
         );
         await pingJiraInstance(jiraOptions.url, credentials);
         const jiraClient = new JiraClientCloud(jiraOptions.url, credentials);
-        if (ENV_XRAY_CLIENT_ID in env && ENV_XRAY_CLIENT_SECRET in env) {
+        if (
+            ENV_NAMES.authentication.xray.clientId in env &&
+            ENV_NAMES.authentication.xray.clientSecret in env
+        ) {
             // Xray cloud authentication: client ID and client secret.
             logInfo(
                 "Xray client ID and client secret found. Setting up Xray cloud JWT credentials"
             );
             const xrayCredentials = new JWTCredentials(
-                env[ENV_XRAY_CLIENT_ID],
-                env[ENV_XRAY_CLIENT_SECRET],
+                env[ENV_NAMES.authentication.xray.clientId],
+                env[ENV_NAMES.authentication.xray.clientSecret],
                 `${XrayClientCloud.URL}/authenticate`
             );
             await pingXrayCloud(xrayCredentials);
@@ -355,10 +330,10 @@ export async function initClients(
                 `)
             );
         }
-    } else if (ENV_JIRA_API_TOKEN in env && jiraOptions.url) {
+    } else if (ENV_NAMES.authentication.jira.apiToken in env && jiraOptions.url) {
         // Jira server authentication: no username, only token.
         logInfo("Jira PAT found. Setting up Jira server PAT credentials");
-        const credentials = new PATCredentials(env[ENV_JIRA_API_TOKEN]);
+        const credentials = new PATCredentials(env[ENV_NAMES.authentication.jira.apiToken]);
         await pingJiraInstance(jiraOptions.url, credentials);
         const jiraClient = new JiraClientServer(jiraOptions.url, credentials);
         // Xray server authentication: no username, only token.
@@ -377,11 +352,15 @@ export async function initClients(
             xrayClient: xrayClient,
             jiraRepository: new CachingJiraRepository(jiraFieldRepository, jiraFieldFetcher),
         };
-    } else if (ENV_JIRA_USERNAME in env && ENV_JIRA_PASSWORD in env && jiraOptions.url) {
+    } else if (
+        ENV_NAMES.authentication.jira.username in env &&
+        ENV_NAMES.authentication.jira.password in env &&
+        jiraOptions.url
+    ) {
         logInfo("Jira username and password found. Setting up Jira server basic auth credentials");
         const credentials = new BasicAuthCredentials(
-            env[ENV_JIRA_USERNAME],
-            env[ENV_JIRA_PASSWORD]
+            env[ENV_NAMES.authentication.jira.username],
+            env[ENV_NAMES.authentication.jira.password]
         );
         await pingJiraInstance(jiraOptions.url, credentials);
         const jiraClient = new JiraClientServer(jiraOptions.url, credentials);
