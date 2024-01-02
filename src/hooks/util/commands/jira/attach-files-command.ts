3481f203ba4b3d782aa4f847c522540d4b67084d
Sebastian Vollbrecht <sebivo@freenet.de>
2024-01-03 00:50:40 +0100

    Add tests

--- src/hooks/util/commands/jira/attach-files-command.ts
+++ src/hooks/util/commands/jira/attach-files-command.ts
@@ -6,23 +6,30 @@ import { Command, Computable, SkippedError } from "../../../command";
 export class AttachFilesCommand extends Command<Attachment[]> {
     private readonly jiraClient: JiraClient;
     private readonly files: Computable<string[]>;
-    private readonly issueKey: Computable<string>;
-    constructor(jiraClient: JiraClient, files: Computable<string[]>, issueKey: Computable<string>) {
+    private readonly resolvedExecutionIssueKey: Computable<string>;
+    constructor(
+        jiraClient: JiraClient,
+        files: Computable<string[]>,
+        resolvedExecutionIssueKey: Computable<string>
+    ) {
         super();
         this.jiraClient = jiraClient;
         this.files = files;
-        this.issueKey = issueKey;
+        this.resolvedExecutionIssueKey = resolvedExecutionIssueKey;
     }
 
     protected async computeResult(): Promise<Attachment[]> {
-        const executionIssueKey = await this.issueKey.compute();
+        const resolvedExecutionIssueKey = await this.resolvedExecutionIssueKey.compute();
         const files = await this.files.compute();
         if (files.length === 0) {
             throw new SkippedError(
-                `Skipping attaching files to test execution issue ${executionIssueKey}: No files to attach`
+                `Skipping attaching files to test execution issue ${resolvedExecutionIssueKey}: No files to attach`
             );
         }
-        LOG.message(Level.INFO, `Attaching files to text execution issue ${executionIssueKey}`);
-        return await this.jiraClient.addAttachment(executionIssueKey, ...files);
+        LOG.message(
+            Level.INFO,
+            `Attaching files to test execution issue ${resolvedExecutionIssueKey}`
+        );
+        return await this.jiraClient.addAttachment(resolvedExecutionIssueKey, ...files);
     }
 }
