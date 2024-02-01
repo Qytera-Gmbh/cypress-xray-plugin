ab8113b59fbf861ef5b2cb962a6affba1d1f594b
Sebastian Vollbrecht <sebivo@freenet.de>
2024-02-01 20:40:43 +0100

    Switch to d3

--- src/util/graph/visualisation/d3.ts
+++ src/util/graph/visualisation/d3.ts
@@ -1,39 +1,36 @@
 import { unknownToString } from "../../string";
 import { DirectedEdge, DirectedGraph } from "../graph";
 
-interface MermaidVertex {
+interface D3Vertex {
     id: string;
     name: string;
     parameters: string;
     result: string;
-    showContent: false;
-    classDef?: string;
+    state: string;
 }
 
-interface MermaidEdge {
+interface D3Edge {
     src: string;
     dst: string;
-    style: "normal" | "dotted" | "thick";
+    style: "normal" | "dotted";
 }
 
-interface MermaidJson {
-    vertices: MermaidVertex[];
-    edges: MermaidEdge[];
-    classDefs: Record<string, string>;
+interface D3Json {
+    vertices: D3Vertex[];
+    edges: D3Edge[];
 }
 
-export async function graphToMermaidJson<V, E extends DirectedEdge<V>>(
+export async function graphToD3Json<V, E extends DirectedEdge<V>>(
     graph: DirectedGraph<V, E>,
     getName: (vertex: V) => string | Promise<string>,
     getParameters: (vertex: V) => string | Promise<string>,
     getResults: (vertex: V) => string | Promise<string>,
-    getClass: (vertex: V) => string | Promise<string>,
-    getEdgeStyle: (edge: DirectedEdge<V>) => "normal" | "dotted" | "thick"
-): Promise<MermaidJson> {
-    const json: MermaidJson = {
+    getState: (vertex: V) => string | Promise<string>,
+    getEdgeStyle: (edge: DirectedEdge<V>) => "normal" | "dotted"
+): Promise<D3Json> {
+    const json: D3Json = {
         vertices: [],
         edges: [],
-        classDefs: {},
     };
     const ids = new Map<V, string>();
     const cssClasses = new Map<string, Set<V>>();
@@ -41,23 +38,21 @@ export async function graphToMermaidJson<V, E extends DirectedEdge<V>>(
     for (const vertex of graph.getVertices()) {
         const vertexId = `v${ids.size}`;
         ids.set(vertex, vertexId);
-        const vertexClass = await getClass(vertex);
-        const vertices = cssClasses.get(vertexClass);
+        const vertexState = await getState(vertex);
+        const vertices = cssClasses.get(vertexState);
         if (vertices) {
             vertices.add(vertex);
         } else {
             const classId = `c${cssClassIds.size}`;
-            cssClasses.set(vertexClass, new Set([vertex]));
-            cssClassIds.set(vertexClass, classId);
-            json.classDefs[classId] = vertexClass;
+            cssClasses.set(vertexState, new Set([vertex]));
+            cssClassIds.set(vertexState, classId);
         }
         json.vertices.push({
             id: vertexId,
             name: await getName(vertex),
             parameters: await getParameters(vertex),
             result: await getResults(vertex),
-            showContent: false,
-            classDef: cssClassIds.get(vertexClass),
+            state: vertexState,
         });
     }
     for (const edge of graph.getEdges()) {
