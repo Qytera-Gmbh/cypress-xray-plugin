0662e168323ae615cc80375c18cf5a0a194cb7e2
Sebastian Vollbrecht <sebivo@freenet.de>
2024-08-11 13:18:13 +0200

    Fix server response

--- test/server.ts
+++ test/server.ts
@@ -4,9 +4,9 @@ import { LOCAL_SERVER } from "./server-config";
 const SERVER = http.createServer((req, res) => {
     res.statusCode = 200;
     res.setHeader("Access-Control-Expose-Headers", "X-Response-Time");
-    res.setHeader("Content-Type", "text/plain");
+    res.setHeader("Content-Type", "text/html");
     res.setHeader("X-Response-Time", Date.now());
-    res.end("Hello World\n");
+    res.end("<html>Hello World</html>");
 });
 
 SERVER.listen(LOCAL_SERVER.port, () => {
