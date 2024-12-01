a5421305e73c3b25b9e8ed81f06b2d2429e68dac
Sebastian Vollbrecht <sebivo@freenet.de>
2024-12-01 20:07:32 +0100

    Fix tests

--- src/hooks/after/commands/extract-video-files-command.test.ts
+++ src/hooks/after/commands/extract-video-files-command.test.ts
@@ -1,39 +1,33 @@
-import { expect } from "chai";
+import assert from "node:assert";
 import fs from "node:fs";
 import { relative } from "node:path";
 import { cwd } from "node:process";
 import { describe, it } from "node:test";
-import { getMockedLogger } from "../../../../test/mocks.js";
 import type { CypressRunResultType } from "../../../types/cypress/cypress.js";
+import { LOG } from "../../../util/logging.js";
 import { ConstantCommand } from "../../util/commands/constant-command.js";
 import { ExtractVideoFilesCommand } from "./extract-video-files-command.js";
 
 await describe(relative(cwd(), import.meta.filename), async () => {
     await describe(ExtractVideoFilesCommand.name, async () => {
-        await it("extracts video files", async () => {
-            const logger = getMockedLogger();
+        await it("extracts video files", async (context) => {
+            context.mock.method(LOG, "message", context.mock.fn());
             const runResults = JSON.parse(
                 fs.readFileSync("./test/resources/runResult.json", "utf-8")
             ) as CypressRunResultType;
-            const command = new ExtractVideoFilesCommand(
-                logger,
-                new ConstantCommand(logger, runResults)
-            );
-            expect(await command.compute()).to.deep.eq([
+            const command = new ExtractVideoFilesCommand(LOG, new ConstantCommand(LOG, runResults));
+            assert.deepStrictEqual(await command.compute(), [
                 "~/repositories/xray/cypress/videos/example.cy.ts.mp4",
             ]);
         });
 
-        await it("skips null paths", async () => {
-            const logger = getMockedLogger();
+        await it("skips null paths", async (context) => {
+            context.mock.method(LOG, "message", context.mock.fn());
             const runResults = JSON.parse(
                 fs.readFileSync("./test/resources/runResult_13_0_0.json", "utf-8")
             ) as CypressRunResultType;
-            const command = new ExtractVideoFilesCommand(
-                logger,
-                new ConstantCommand(logger, runResults)
-            );
-            expect(await command.compute()).to.deep.eq([]);
+            const command = new ExtractVideoFilesCommand(LOG, new ConstantCommand(LOG, runResults));
+            assert.deepStrictEqual(await command.compute(), []);
         });
     });
 });
