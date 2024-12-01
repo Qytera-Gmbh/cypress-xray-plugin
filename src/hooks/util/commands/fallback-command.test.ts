c056fddf07f3555642f8136224aa42e3516c3489
Sebastian Vollbrecht <sebivo@freenet.de>
2024-12-01 22:18:45 +0100

    Fix tests

--- src/hooks/util/commands/fallback-command.test.ts
+++ src/hooks/util/commands/fallback-command.test.ts
@@ -1,41 +1,41 @@
-import { expect } from "chai";
+import assert from "node:assert";
 import { relative } from "node:path";
 import { cwd } from "node:process";
 import { describe, it } from "node:test";
-import { getMockedLogger } from "../../../../test/mocks.js";
+import { LOG } from "../../../util/logging.js";
 import { ComputableState } from "../../command.js";
 import { ConstantCommand } from "./constant-command.js";
 import { FallbackCommand } from "./fallback-command.js";
 
 await describe(relative(cwd(), import.meta.filename), async () => {
     await describe(FallbackCommand.name, async () => {
-        await it("computes the result if possible", async () => {
-            const logger = getMockedLogger();
-            const input = new ConstantCommand(logger, 42);
+        await it("computes the result if possible", async (context) => {
+            context.mock.method(LOG, "message", context.mock.fn());
+            const input = new ConstantCommand(LOG, 42);
             const command = new FallbackCommand(
                 {
                     fallbackOn: [ComputableState.FAILED],
                     fallbackValue: "fallback",
                 },
-                logger,
+                LOG,
                 input
             );
-            expect(await command.compute()).to.eq(42);
+            assert.strictEqual(await command.compute(), 42);
         });
 
-        await it("returns the fallback value", async () => {
-            const logger = getMockedLogger();
-            const input = new ConstantCommand(logger, 42);
+        await it("returns the fallback value", async (context) => {
+            context.mock.method(LOG, "message", context.mock.fn());
+            const input = new ConstantCommand(LOG, 42);
             const command = new FallbackCommand(
                 {
                     fallbackOn: [ComputableState.FAILED],
                     fallbackValue: "fallback",
                 },
-                logger,
+                LOG,
                 input
             );
             input.setState(ComputableState.FAILED);
-            expect(await command.compute()).to.eq("fallback");
+            assert.strictEqual(await command.compute(), "fallback");
         });
     });
 });
