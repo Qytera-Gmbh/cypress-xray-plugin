4c142ed47dc03e4bfce110424c44ebc8f4c5e5f4
Sebastian Vollbrecht <sebivo@freenet.de>
2024-12-01 21:06:14 +0100

    Fix tests

--- src/hooks/preprocessor/commands/parse-feature-file-command.test.ts
+++ src/hooks/preprocessor/commands/parse-feature-file-command.test.ts
@@ -1,33 +1,32 @@
-import { expect } from "chai";
+import assert from "node:assert";
 import { relative } from "node:path";
 import { cwd } from "node:process";
 import { describe, it } from "node:test";
-import { getMockedLogger } from "../../../../test/mocks.js";
 import { dedent } from "../../../util/dedent.js";
-import { Level } from "../../../util/logging.js";
+import { Level, LOG } from "../../../util/logging.js";
 import { ParseFeatureFileCommand } from "./parse-feature-file-command.js";
 
 await describe(relative(cwd(), import.meta.filename), async () => {
     await describe(ParseFeatureFileCommand.name, async () => {
-        await it("displays errors for invalid feature files", async () => {
-            const logger = getMockedLogger();
+        await it("displays errors for invalid feature files", async (context) => {
+            const message = context.mock.method(LOG, "message", context.mock.fn());
             const filePath = "./test/resources/features/invalid.feature";
 
-            const command = new ParseFeatureFileCommand({ filePath: filePath }, logger);
-            await expect(command.compute()).to.eventually.be.rejectedWith(
-                dedent(`
+            const command = new ParseFeatureFileCommand({ filePath: filePath }, LOG);
+            await assert.rejects(command.compute(), {
+                message: dedent(`
                     ./test/resources/features/invalid.feature
 
                       Failed to parse feature file.
 
                         Parser errors:
                         (9:3): expected: #EOF, #TableRow, #DocStringSeparator, #StepLine, #TagLine, #ScenarioLine, #RuleLine, #Comment, #Empty, got 'Invalid: Element'
-                `)
-            );
-            expect(logger.message).to.have.been.calledWithExactly(
+                `),
+            });
+            assert.deepStrictEqual(message.mock.calls[0].arguments, [
                 Level.INFO,
-                `Parsing feature file: ./test/resources/features/invalid.feature`
-            );
+                `Parsing feature file: ./test/resources/features/invalid.feature`,
+            ]);
         });
     });
 });
