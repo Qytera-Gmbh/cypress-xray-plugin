be3ea17adb722c7267b5490911d2b7f67f2f7f98
Sebastian Vollbrecht <sebivo@freenet.de>
2025-03-08 03:40:46 +0100

    Apply last attempt filtering to manual screenshots as well

--- src/hooks/after/commands/conversion/cypress/util/converter.ts
+++ src/hooks/after/commands/conversion/cypress/util/converter.ts
@@ -251,22 +251,18 @@ export class RunConverterV13 implements RunConverter {
     public getNonAttributableScreenshots(options: { onlyLastAttempt: boolean }): string[] {
         let screenshots = this.runResults
             .flatMap((run) => run.screenshots)
-            .map((screenshot) => screenshot.path);
+            .map((screenshot) => screenshot);
         if (options.onlyLastAttempt) {
-            screenshots = screenshots.filter(
-                (screenshot) =>
-                    !this.isAttemptScreenshot(screenshot) ||
-                    this.isLastAttemptScreenshot(screenshot)
-            );
+            screenshots = this.filterLastAttemptScreenshots(screenshots);
         }
         screenshots = screenshots.filter((screenshot) => {
             try {
-                return getTestIssueKeys(screenshot, this.projectKey).length === 0;
+                return getTestIssueKeys(screenshot.path, this.projectKey).length === 0;
             } catch {
                 return true;
             }
         });
-        return screenshots;
+        return [...new Set([...screenshots.map((screenshot) => screenshot.path)])];
     }
 
     public getScreenshots(issueKey: string, options: { onlyLastAttempt: boolean }): string[] {
@@ -276,60 +272,75 @@ export class RunConverterV13 implements RunConverter {
                 if (!screenshot.path.includes(issueKey)) {
                     continue;
                 }
-                screenshots.push(screenshot.path);
+                screenshots.push(screenshot);
             }
         }
         if (options.onlyLastAttempt) {
-            screenshots = screenshots.filter(
-                (screenshot) =>
-                    !this.isAttemptScreenshot(screenshot) ||
-                    this.isLastAttemptScreenshot(screenshot)
-            );
+            screenshots = this.filterLastAttemptScreenshots(screenshots);
         }
-        return [...new Set([...screenshots])];
+        return [...new Set([...screenshots.map((screenshot) => screenshot.path)])];
     }
 
-    private isAttemptScreenshot(screenshotPath: string) {
-        const filename = basename(screenshotPath, extname(screenshotPath));
-        for (const run of this.runResults) {
-            for (const test of run.tests) {
+    private filterLastAttemptScreenshots(screenshots: CypressCommandLine.ScreenshotInformation[]) {
+        // See: https://docs.cypress.io/app/guides/test-retries#Screenshots
+        // Manual screenshots:
+        //     Initial run: CYP-123 my screenshot.png
+        //           Retry: CYP-123 my screenshot (attempt 2).png
+        //           Retry: CYP-123 my screenshot (attempt 3).png
+        // Cypress screenshots:
+        //     Initial run: CYP-123 test passes eventually (failed).png
+        //           Retry: CYP-123 test passes eventually (failed) (attempt 2).png
+        //           Retry: <no screenshot because attempt passed>
+        const attemptRegex = /\(attempt (\d+)\)/;
+        const initialScreenshots = screenshots.filter(
+            (screenshot) => !attemptRegex.exec(screenshot.path)
+        );
+        const lastScreenshots = [];
+        // Sort screenshots such that the highest attempt always comes first.
+        for (const initialScreenshot of initialScreenshots) {
+            const name = basename(initialScreenshot.path, extname(initialScreenshot.path));
+            const similarScreenshots = screenshots.filter((screenshot) =>
+                screenshot.path.includes(name)
+            );
+            similarScreenshots.sort((a, b) => {
+                const matchA = attemptRegex.exec(a.path);
+                const matchB = attemptRegex.exec(b.path);
+                if (matchA && matchB) {
+                    return Number.parseInt(matchB[1]) - Number.parseInt(matchA[1]);
+                }
+                if (matchA) {
+                    return -1;
+                }
+                if (matchB) {
+                    return 1;
+                }
+                return 0;
+            });
+            lastScreenshots.push(similarScreenshots[0]);
+        }
+        // Remove all screenshots of failed attempts that have been superseded by a passed one
+        // without screenshot.
+        const tests = this.runResults.flatMap((result) => result.tests);
+        const testScreenshotPatterns = tests.map(
+            (test) =>
+                new RegExp(
+                    `${test.title[test.title.length - 1]} \\(${[CypressStatus.FAILED, CypressStatus.PASSED, CypressStatus.PENDING, CypressStatus.SKIPPED].join("|")}\\)`
+                )
+        );
+        return lastScreenshots.filter((screenshot) => {
+            if (!testScreenshotPatterns.some((pattern) => pattern.exec(screenshot.path))) {
+                return true;
+            }
+            for (const test of tests) {
                 const testName = test.title[test.title.length - 1];
-                // See: https://docs.cypress.io/app/guides/test-retries#Screenshots
-                // Initial run: test passes eventually (failed).png
-                //       Retry: test passes eventually (failed) (attempt 2).png
-                for (const status of [
-                    CypressStatus.FAILED,
-                    CypressStatus.PASSED,
-                    CypressStatus.PENDING,
-                    CypressStatus.SKIPPED,
-                ]) {
-                    const attemptFilename = `${testName} (${status})`;
-                    if (filename.includes(attemptFilename)) {
+                if (test.attempts.length > 1) {
+                    const finalAttemptName = `${testName} (${test.state}) (attempt ${test.attempts.length.toString()})${extname(screenshot.path)}`;
+                    if (screenshot.path.endsWith(finalAttemptName)) {
                         return true;
                     }
                 }
             }
-        }
-        return false;
-    }
-
-    private isLastAttemptScreenshot(screenshotPath: string) {
-        const filename = basename(screenshotPath, extname(screenshotPath));
-        for (const run of this.runResults) {
-            for (const test of run.tests) {
-                const testName = test.title[test.title.length - 1];
-                // See: https://docs.cypress.io/app/guides/test-retries#Screenshots
-                // Initial run: test passes eventually (failed).png
-                //       Retry: test passes eventually (failed) (attempt 2).png
-                let attemptFilename = `${testName} (${test.state})`;
-                if (test.attempts.length > 1) {
-                    attemptFilename = `${attemptFilename} (attempt ${test.attempts.length.toString()})`;
-                }
-                if (filename.endsWith(attemptFilename)) {
-                    return true;
-                }
-            }
-        }
-        return false;
+            return false;
+        });
     }
 }
